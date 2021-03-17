123115d5bff89a65d788c8c073d2337e47756e5f
Julian Burgess <aubergene@gmail.com>
2021-03-17 21:05:00 +0000

    Updates

---
+++ client/legacy/constants.1c34ffaf.js
@@ -0,0 +1,1 @@
+var t="third___.goldcomparts.show",a="https://".concat("third___.goldcomparts.show"),o="https://www.eventbrite.co.uk/e/chimera-garden-computational-arts-mamfa-degree-show-digital-exhibition-tickets-119608075975";export{a as B,o as E,t as a};
--- client/legacy/constants.6c819469.js
+++
@@ -1,1 +0,0 @@
-var a="2021-undergrad.goldcomparts.show",t="https://".concat(a),e="https://www.eventbrite.co.uk/e/chimera-garden-computational-arts-mamfa-degree-show-digital-exhibition-tickets-119608075975";export{t as B,e as E,a};
