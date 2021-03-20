fd1a583ec6a55c278a34e14b2a8c18e11ce84630
Matthew Heywood <mheyw001@gold.ac.uk>
2021-03-20 16:50:38 +0000

    added interactive position statement and alt text with ARIA settings to ignore interactive version on screen reader

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -52,19 +52,14 @@
               <h2 class="subtitle"> Digital Arts Computating Degree Show</h2>
               <h2 class="location">Goldsmiths, University of London</h2>
             </div>
-            <div class="introBodyStrong">
-              Third ____ brings together a c✴nstellati✴n of works which have escaped their tethers to a [material conception]1 or [ideological representation]2 of space to approach  an[other]3 third space that overcomes the bina⚭ries of the first two, where ⇶linkages can be made in an infin∞ number of ways. As such, the title is never static but is alive, ever-changing and evolving to reflect the simultaneously inward juxtapositi⊗n and outward distributi⊙n of art and technology. ____ is not an invitation to be fill▥ed nor is it an open-ended ⸮peculation into the future. It is a transparent vision to blast spectrums of knowable⟷unimaginable, structure⟷agency, everyday life⟷unending history out of chronological ⇄ continuity into a 3√radical dimension beyond an empirical or manifest order of knowledge.
-            </div>
-            <div>
-              <a href="/schedule" class="rounded-link col-7 bd-col-7">
-                EVENTS SCHEDULE
-              </a>
-              <a href="/getting-there" class="rounded-link col-7 bd-col-7">
-                VISIT ON-SITE
-              </a>
+            <div class="introBodyStrong" aria-hidden="true">
+              Third ___  brings together a <b1 class="psb"><span>c✴︎nstillati✴︎n</span></b1> of works which have escaped their tethers to a [material conception]₁ or [ideological representation]₂ of space to approach an[other]₃ third space where <b2 class="psb"><span>⇶linkages</span></b2> can be made in an <b3 class="psb"><span>infin∞</span></b3> number of ways. As such, the <b4 class="psb"><span>gateway⍈</span></b4> is never static but is alive, ever-changing and evolving to reflect the simultaneously inward <b5 class="psb"><span>juxtapositi⊗n</span></b5> and outward <b6 class="psb"><span>distributi⊙n</span></b6> of art and technology. ___ is not an invitation to be <b7 class="psb"><span>fill▥ed</span></b7> nor is it an open-ended vision into the future. Offering up a <b8 class="psb"><span>⸮peculative</span></b8> archive, Third ____ operates as a <b9 class="psb"><span>para||e|</span></b9> world of unstable states and dual truths; disrupting <b10 class="psb"><span>bina⚭ries</span></b10> of the knowable<b11 class="psb"><span>⟷</span></b11>unimaginable, structure<b12 class="psb"><span>⟷</span></b12>agency, everyday life<b13 class="psb"><span>⟷</span></b13>unending history out of <b14 class="psb"><span>chronological ⇄ continuity</span></b14>. A <b15 class="psb"><span>³√radical</span></b15> dimension beyond an empirical or manifest order of knowledge.
             </div>
           </div>
         </div>
+        <div class="introAltText" role="text">
+              (alt-text)<br>Third ___ brings together a constellation of works which have escaped their tethers to a material conception or ideological representation of space, to approach an(other) third space where linkages can be made in an infinite number of ways. As such, this gateway is never static but is alive, ever-changing and evolving to reflect the simultaneously inward juxtaposition and outward distribution of art and technology. ___ is not an invitation to be filled nor is it an open-ended vision into the future. Offering up a speculative archive, Third ___ operates as a parallel world of unstable states and dual truths; disrupting binaries of the knowable and the unimaginable, structure and agency, everyday life and unending history out of chronological continuity. A radical dimension beyond an empirical or manifest order of knowledge.
+            </div>
       </div>
     </div>
   </section>
@@ -103,6 +98,14 @@
     color: #fcfcfc;
     padding: 2rem 0rem;
   }
+  .introAltText {
+    font-family: futura-pt, sans-serif;
+    font-weight: 400;
+    font-size: 0.9rem;
+    line-height: 1rem;
+    color: #fcfcfc;
+    padding: 2rem 0rem;
+  }
   .leftColumn {
     min-height: 700px;
     max-height: 1500px;
@@ -121,4 +124,56 @@
       padding-top: 0rem;
     }
   }
+
+  /*Positioning Text Interaction*/
+  .psb {
+  background: none!important;
+  border: none;
+  padding: 0!important;
+}
+/* constillation */
+b1:hover span {display:none}
+b1:hover:before {content:"constillation"}
+/* linkages */
+b2:hover span {display:none}
+b2:hover:before {content:"linkages"}
+/* infinite */
+b3:hover span {display:none}
+b3:hover:before {content:"infinite"}
+/* gateway */
+b4:hover span {display:none}
+b4:hover:before {content:"gateway"}
+/* juxtaposition */
+b5:hover span {display:none}
+b5:hover:before {content:"juxtaposition"}
+/* distribution */
+b6:hover span {display:none}
+b6:hover:before {content:"distribution"}
+/* filled */
+b7:hover span {display:none}
+b7:hover:before {content:"filled"}
+/* speculative */
+b8:hover span {display:none}
+b8:hover:before {content:"speculative"}
+/* parallel */
+b9:hover span {display:none}
+b9:hover:before {content:"parallel"}
+/* binaries */
+b10:hover span {display:none}
+b10:hover:before {content:"binaries"}
+/* and the */
+b11:hover span {display:none}
+b11:hover:before {content:" and the "}
+/* and */
+b12:hover span {display:none}
+b12:hover:before {content:" and "}
+/* and */
+b13:hover span {display:none}
+b13:hover:before {content:" and "}
+/* chronological continuity */
+b14:hover span {display:none}
+b14:hover:before {content:"chronological continuity"}
+/* radical */
+b15:hover span {display:none}
+b15:hover:before {content:"radical"}
 </style>
